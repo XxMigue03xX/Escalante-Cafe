91584279a06e594f1bd1718c4aca5568373256e8
Miguel Garavito <mgaravitocamargo@gmail.com>
2024-04-05 15:49:19 -0500

    nav replaced

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,38 @@
+import logo from "../../assets/logo.png";
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <nav className="nav-container">
+      <div className="nav__section1">
+        <div className="nav__phone-container">
+          <i className="bx bxs-phone"></i>
+        </div>
+        <a
+            href="https://wa.me/+573202808051?text=Hola,%20ví%20la%20pagina%20de escalante%20café%20y%20quiero%20contactar%20con%20ustedes"
+            target="_blank"
+            rel="noreferrer"
+            className="nav__phone-link">
+            +57 3202808051
+        </a>
+        <p>
+        Para venta de Café tostado y exportaciones
+        </p>
+      </div>
+      <div className="nav__section2">
+        <div className="nav__logo-container">
+          <img src={logo} alt="logoEscalante" />
+        </div>
+        <div className="nav__options-container">
+            <ul className="nav__options-list">
+                <li>
+                  <p className="nav__option-inicio">Inicio</p>
+                </li>
+            </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
